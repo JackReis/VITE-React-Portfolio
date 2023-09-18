04c5c5bf352e7e5ccb25ea7bc625a3e651d5c864
Jack Reis <JackAReis@gmail.com>
2023-09-18 12:14:02 -0700

    Update Contact.jsx with emailjs Gmail
    Default template

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,16 +29,16 @@ const Contact = () => {
     //click on create a new template then click on save.
     emailjs
       .send(
-        'serviceID', // paste your ServiceID here (you'll get one when your service is created).
-        'templateID', // paste your TemplateID here (you'll find it under email templates).
+        'service_5ud5pfl', // paste your ServiceID here (you'll get one when your service is created).
+        'template_o7xmfe4', // paste your TemplateID here (you'll find it under email templates).
         {
           from_name: form.name,
-          to_name: 'YourName', // put your name here.
+          to_name: 'Jack Reis', // put your name here.
           from_email: form.email,
-          to_email: 'youremail@gmail.com', //put your email here.
+          to_email: 'jackareis@gmail.com', //put your email here.
           message: form.message,
         },
-        'yourpublickey' //paste your Public Key here. You'll get it in your profile section.
+        'sE-4Zs22SzM7T9x79' //paste your Public Key here. You'll get it in your profile section.
       )
       .then(
         () => {
