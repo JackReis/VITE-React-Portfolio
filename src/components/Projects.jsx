29f37164bb5cc12bda50cc5a01888f3c834c4e75
Jack Reis <JackAReis@gmail.com>
2023-09-18 12:01:27 -0700

    Update Projects.jsx
    Rewrite projects description

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -124,11 +124,7 @@ const Projects = () => {
         <motion.p
           variants={fadeIn('', '', 0.1, 1)}
           className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
-          These projects demonstrate my expertise with practical examples of
-          some of my work, including brief descriptions and links to code
-          repositories and live demos. They showcase my ability to tackle
-          intricate challenges, adapt to various technologies, and efficiently
-          oversee projects.
+          These projects are a few examples of what has been intriguing me lately.
         </motion.p>
       </div>
 
