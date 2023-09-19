c6c54176eeda1157bd96085a9595cd821c1f6a5b
Jack Reis <JackAReis@gmail.com>
2023-09-19 12:59:10 -0700

    ⛳️ Update Hero.jsx with new text for Bio and About
    Ported text linking to Reis font, Bio and About. Unstyled

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,17 +37,21 @@ const Hero = () => {
           <div>
             <h1
               className={`${styles.heroHeadText} text-eerieBlack font-poppins uppercase`}>
-              Hi, I'm{' '}
+              I, {' '}, am a for-hire Product Design Scientist.
               <span
                 className="sm:text-battleGray sm:text-[90px] 
                 text-eerieBlack text-[50px] font-mova
                 font-extrabold uppercase">
-                Jack
+                Jack Reis
               </span>
             </h1>
-            <p className={`${styles.heroSubText} mt-2 text-eerieBlack`}>
-              I craft valuable, intuitive, and enjoyable digital product experiences for my clients and their communities.<br className="sm:block hidden" />
+            <p className={`${styles.heroSubText} mt-2 text-eerieBlack`}>I am also working on the next version of <a href="https://www.github.com/JackReis/Reis">Reis</a>, a typeface I created from my father's handwriting.</p>
+            <p className={`${styles.heroSubText} mt-2 text-eerieBlack>Over the last 12 years, I’ve also held roles as a Design Technologist, System Educator, and Data Viz Lead.</p>
+            <p className={`${styles.heroSubText} mt-2 text-eerieBlack>I blend practical considerations with a natural inclination toward beauty and simplicity to craft valuable, intuitive, and enjoyable digital product experiences for my clients and their communities.<br className="sm:block hidden" />
             </p>
+            <p className={`${styles.heroSubText} mt-2 text-eerieBlack>My most scaled project was at Google on the Nest smart home interfaces; Google holds more than 20% of the global smart speaker market and sold 50 million Google Home devices worldwide in 2021.</p>
+            <p className={`${styles.heroSubText} mt-2 text-eerieBlack>While digital craft holds the majority of my attention, I am also a committed husband, trained yoga instructor, and lifelong learner.</p>
+            <p className={`${styles.heroSubText} mt-2 text-eerieBlack>What fulfills me most is finding new, better ways to be and do things.</p>
           </div>
           <div
             className="w-screen flex flex-col items-start 
